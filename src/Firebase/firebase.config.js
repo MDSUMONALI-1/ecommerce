e5eebedf7d84c100e3339bf8570a16f59ea51058
unknown <mdsumonaliceng@gmail.com>
2021-11-26 16:36:19 +0600

    1

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAhUiU-29ocnvY3Idl2wEpRE22w6jBKNJ4",
+    authDomain: "ema-john-simple-e4799.firebaseapp.com",
+    projectId: "ema-john-simple-e4799",
+    storageBucket: "ema-john-simple-e4799.appspot.com",
+    messagingSenderId: "737284861098",
+    appId: "1:737284861098:web:5263a5db1ff53972a0f4d3"
+  };
+
+export default firebaseConfig;
